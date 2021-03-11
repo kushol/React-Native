69d92a5220ea5e2a2937f685cee1086e8445d801
Bayzid <ashik1876@gmail.com>
2021-03-11 23:03:48 +1100

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
-//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
-
-
-import AppButton from './app/component/AppButton';
-import AppText from './app/component/AppText';
-import AppColor from './app/config/AppColor';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppButton title="Click here"></AppButton>    
-    </View>
+   
+    <WelcomeScreen/>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: AppColor.otherColor,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{
+    backgroundColor: 'lightblue',
+
+  }
+}
+
+);
